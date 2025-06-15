196dea9ed0537184004eca1bd817f127b2fc2c46
NikolaiSemko <semk0001@algonquinlive.com>
2025-06-15 17:19:13 -0400

    before first test

--- outputs.tf
+++ outputs.tf
@@ -12,5 +12,5 @@ output "nic_name" {
 }
 
 output "public_ip" {
-  value = azurerm_linux_virtual_machine.webserver.public_ip_address
+  value = azurerm_public_ip.webserver.ip_address  # CHANGED - get IP from public_ip resource
 }
